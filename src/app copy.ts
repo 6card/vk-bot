8511255a404952731f5c90c29ad165c9925f0bc2
6card <vk_31@mail.ru>
2021-03-05 18:19:20 +0300

    save

--- src/app copy.ts
+++ src/app copy.ts
@@ -1,26 +1,21 @@
-import { config } from './config';
+import { token } from './config';
 
 import express, { Request, Response } from 'express'
 import { Telegraf } from 'telegraf'
 
-const token = config.token
+// const token = process.env.BOT_TOKEN
 if (token === undefined) {
   throw new Error('BOT_TOKEN must be provided!')
 }
 
 const bot = new Telegraf(token)
 // Set the bot response
-bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello!</b>'))
+bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
 
 // Set telegram webhook
 // npm install -g localtunnel && lt --port 5001
-//bot.telegram.setWebhook('https://bot.6card.ru/vk')
-bot.launch({webhook: {
-  domain: 'https://bot.6card.ru',
-  hookPath: '/vk',
-  port: 5001,
-}})
-/*
+bot.telegram.setWebhook('https://bot.6card.ru/vk')
+
 const app = express()
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 // Set the bot API endpoint
@@ -28,5 +23,3 @@ app.use(bot.webhookCallback('/vk'))
 app.listen(5001, () => {
   console.log('Example app listening on port 5001!')
 })
-
-*/
